c6e317b9a3e56cf259b0d8abc255c014902445fc
jjward-usgs <jjward@usgs.gov>
2017-01-11 15:50:36 -0800

    initial commit

--- src/htdocs/earthquakes/events/1868calif/index.php
+++ src/htdocs/earthquakes/events/1868calif/index.php
@@ -5,19 +5,27 @@
   if  (!isset($TEMPLATE))  {
     $TITLE  =  'October 21, 1868 Hayward Fault Earthquake';
     $HEAD = '
-
+      <link rel="stylesheet" href="style.css">
     ';
     $NAVIGATION  = true;
     include  'template.inc.php';
   }
 ?>
 
+<blockquote>
+	Because of its location in the heart of the Bay Area, then having a total
+	population of about 260,000, and its magnitude, variously estimated as
+	between 6.8 and 7.0, this earthquake was one of the most destructive in
+	California history. Property loss was extensive and 30 people were killed.
+	<a href="/regional/states/events/1868_10_21.php">Read more</a> &raquo;
+</blockquote>
+
 <ul class="no-style linklist">
   <li>
     <a href="http://pubs.usgs.gov/fs/2008/3019/">
       <h3>The Hayward Fault&mdash;Is It Due for a Repeat of the Powerful 1868
   		Earthquake?</h3>
-      <img src="/regional/nca/1868/images/factsheet.jpg" alt="photo of damaged building"/>
+      <img src="images/factsheet.jpg" alt="photo of damaged building"/>
     </a>
     <p>USGS scientists describe the Hayward fault as a tectonic time bomb, due
 		anytime for another magnitude 6.8 to 7.0 earthquake. Because such a quake
@@ -33,20 +41,21 @@
 
 <ul class="no-style linklist">
   <li>
-    <a href="/regional/nca/1868virtualtour/">
+    <a href="virtualtour/">
       <h3>Virtual Tour of the 1868 Hayward Earthquake in Google Earth&trade;</h3>
-      <img src="/regional/nca/1906/googleearth.jpg" alt="Google Earth Globe"/>
+      <img src="../1906calif/googleearth.jpg" alt="Google Earth Globe"/>
     </a>
     <p>Interactive self-guided tour of San Francisco Bay Area faults and
 	  earthquake history featuring ground-shaking maps, historic photographs,
 	  quotes from earthquake survivors, and more.</p>
   </li>
   <li>
-    <a href="">
-      <h3></h3>
-      <img src="image-file" alt="alt text"/>
+    <a href="/regional/nca/simulations/hayward/">
+      <h3>Hayward Fault Scenario Earthquake Animations</h3>
+      <img src="images/hayward.jpg" alt="Map of Hayward and Rodgers Creek faults"/>
     </a>
-    <p></p>
+    <p>Computer simulations of large, anticipated earthquakes on the Hayward
+		and Rodgers Creek faults.</p>
   </li>
   <li>
     <a href="">
