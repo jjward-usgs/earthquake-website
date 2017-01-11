a51c8322b3528e77fd76aebdaeecb1c37805c0ec
jjward-usgs <jjward@usgs.gov>
2017-01-11 15:02:49 -0800

    first commit

---
+++ src/htdocs/earthquakes/events/1868calif/index.php
@@ -0,0 +1,79 @@
+<?php
+  //  Author:  John Ward
+  //  Contact:
+  //  Last modified: 2017-01-11
+  if  (!isset($TEMPLATE))  {
+    $TITLE  =  'October 21, 1868 Hayward Fault Earthquake';
+    $HEAD = '
+
+    ';
+    $NAVIGATION  = true;
+    include  'template.inc.php';
+  }
+?>
+
+<ul class="no-style linklist">
+  <li>
+    <a href="http://pubs.usgs.gov/fs/2008/3019/">
+      <h3>The Hayward Fault&mdash;Is It Due for a Repeat of the Powerful 1868
+  		Earthquake?</h3>
+      <img src="/regional/nca/1868/images/factsheet.jpg" alt="photo of damaged building"/>
+    </a>
+    <p>USGS scientists describe the Hayward fault as a tectonic time bomb, due
+		anytime for another magnitude 6.8 to 7.0 earthquake. Because such a quake
+		could cause hundreds of deaths, leave thousands homeless, and devastate
+		the region&rsquo;s economy, the USGS and other organizations are working
+		together with new urgency to help prepare Bay Area communities for this
+		certain future quake.</p>
+  </li>
+</ul>
+
+
+<h2>New Science and Products</h2>
+
+<ul class="no-style linklist">
+  <li>
+    <a href="/regional/nca/1868virtualtour/">
+      <h3>Virtual Tour of the 1868 Hayward Earthquake in Google Earth&trade;</h3>
+      <img src="/regional/nca/1906/googleearth.jpg" alt="Google Earth Globe"/>
+    </a>
+    <p>Interactive self-guided tour of San Francisco Bay Area faults and
+	  earthquake history featuring ground-shaking maps, historic photographs,
+	  quotes from earthquake survivors, and more.</p>
+  </li>
+  <li>
+    <a href="">
+      <h3></h3>
+      <img src="image-file" alt="alt text"/>
+    </a>
+    <p></p>
+  </li>
+  <li>
+    <a href="">
+      <h3></h3>
+      <img src="image-file" alt="alt text"/>
+    </a>
+    <p></p>
+  </li>
+  <li>
+    <a href="">
+      <h3></h3>
+      <img src="image-file" alt="alt text"/>
+    </a>
+    <p></p>
+  </li>
+  <li>
+    <a href="">
+      <h3></h3>
+      <img src="image-file" alt="alt text"/>
+    </a>
+    <p></p>
+  </li>
+  <li>
+    <a href="">
+      <h3></h3>
+      <img src="image-file" alt="alt text"/>
+    </a>
+    <p></p>
+  </li>
+</ul>
