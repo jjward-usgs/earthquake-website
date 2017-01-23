9e9625a266303a62502958ff42eaf15e40b902a2
jjward-usgs <jjward@usgs.gov>
2017-01-23 13:03:17 -0800

    changes

--- src/htdocs/earthquakes/events/1868calif/index.php
+++ src/htdocs/earthquakes/events/1868calif/index.php
@@ -64,24 +64,132 @@
 		that can be viewed in Google Earth&trade;</p>
   </li>
   <li>
-    <a href="">
-      <h3></h3>
-      <img src="image-file" alt="alt text"/>
+    <a href="http://pubs.usgs.gov/sim/3045/">
+      <h3>Three-Dimensional Geologic Map of the Hayward Fault Zone</h3>
+      <img src="images/3dmap.gif" alt="Map of the Bay Area"/>
     </a>
-    <p></p>
+    <p>Modified Mercalli Intensity maps for the Hayward earthquake plotted in
+		ShakeMap format.</p>
   </li>
   <li>
-    <a href="">
-      <h3></h3>
-      <img src="image-file" alt="alt text"/>
+    <a href="http://pubs.usgs.gov/of/2008/1121/">
+      <h3>Intensity Maps for the 1868 Hayward Earthquake</h3>
+      <img src="images/1868intensity.jpg" alt="Earthquake Intensity Color Map"/>
     </a>
-    <p></p>
+    <p>Modified Mercalli Intensity maps for the Hayward earthquake plotted in
+		ShakeMap format.</p>
   </li>
   <li>
-    <a href="">
-      <h3></h3>
-      <img src="image-file" alt="alt text"/>
+    <a href="http://geomaps.wr.usgs.gov/gump/people/ponce/hayward_fault/hf.html">
+      <h3>Geometry and Evolution of the Hayward Fault</h3>
+      <img src="images/geometry.jpg" alt="Geometry and Evolution Map"/>
     </a>
-    <p></p>
+    <p>Geophysical data along the Hayward Fault are used to investigate the
+		nature, spatial relationship, and evolution of the Hayward Fault Zone.</p>
+  </li>
+  <li>
+    <a href="/research/structure/3dgeologic/Hayward_geology05.pdf">
+      <h3>Three-dimensional Geologic Map of the Hayward Fault</h3>
+      <img src="1868/images/3d.jpg" alt="3D Image Map of the Hayward Fault"/>
+    </a>
+    <p>A recent paper on the Hayward Fault Model by Russell Graymer and others.
+		Correlation of rock units with variations in seismicity, creep rate, and
+		fault dip (15 MB pdf)</p>
+  </li>
+
+<h2>Field Guides</h2>
+
+  <li>
+    <a href="http://pubs.usgs.gov/of/2008/1135/">
+      <h3>Where&rsquo;s the Hayward Fault? A Green Guide to the Fault</h3>
+      <img src="images/greenguide.jpg" alt="Bay Area Map With Hayward Fault"/>
+    </a>
+    <p>Self-guided field trips to one of North America&rsquo;s most dangerous
+		earthquake faults, the Hayward Fault. Locations were chosen because of
+		their easy access using mass transit and/or their significance relating to
+		the natural and cultural history of the East Bay landscape.</p>
+  </li>
+  <li>
+    <a href="fld007_17e.pdf">
+      <h3>2006 Field Guide to the Hayward Fault (13 MB pdf)</h3>
+      <img src="images/fieldguide.jpg" alt="Field Guide Cover"/>
+    </a>
+    <p>A detailed geologic and architectural field guide to 11 stops along the
+		Hayward Fault, including UC Berkeley Campus and Memorial Stadium, Point
+		Pinole, Cragmont School in Berkeley, Oakland City Hall, and old City Hall
+		in Hayward and it includes discussion of the 1868 earthquake. This guide
+		was published by the Geological Society of America in 2006 as part of the
+		1906 San Francisco Earthquake Centennial Meeting.</p>
+  </li>
+  <li>
+    <a href="http://pubs.usgs.gov/bul/b2188/b2188ch2.pdf">
+      <h3>Field Guide to the Hayward Fault (29 MB pdf)</h3>
+      <img src="images/slide.jpg" alt="Picture 1998 Fremont Peak Landslide"/>
+    </a>
+    <p>Download a 2001 U.S. Geological Survey Field Guide to the Hayward Fault
+		in Downtown Hayward and at the Caldecott Tunnel (between Orinida and
+		Oakland) and to the 1998 Fremont Peak landslide.</p>
+  </li>
+  <li>
+    <a href="http://geomaps.wr.usgs.gov/sfbay/hayward-ft.html">
+      <h3>Field Guide to the Hayward Fault, Downtown Hayward</h3>
+      <img src="images/curb.jpg" alt="Downtown Hayward"/>
+    </a>
+    <p>Explore the Hayward Fault in Hayward using this online field guide
+		prepared for the 2000 U.S. Geological Survey Open House.</p>
+  </li>
+
+<h2>Also See</h2>
+
+  <li>
+    <a href="https://www.eeri.org/projects/earthquake-scenarios/hayward-fault-scenario/">
+      <h3>Hayward Fault Earthquake Loss Scenario</h3>
+      <img src="images/eeri.jpg" alt="Front Cover Showing Hayward"/>
+    </a>
+    <p>This 1996 report (updated in 2010) by the Earthquake Engineering Research Institute
+		describes what losses are expected to result from magnitude 7 earthquake
+		on the northern end of the Hayward Fault.</p>
+  </li>
+  <li>
+    <a href="http://bssa.geoscienceworld.org/site/misc/HaywardConf.xhtml">
+      <h3>Hayward Fault and 1868 Earthquake Special Collection</h3>
+      <img src="" alt=""/>
+    </a>
+    <p>The Seismological Society of America (SSA) created a virtual special
+		issue of BSSA containing previously published BSSA articles focusing on
+		the Hayward earthquake and associated research.</p>
+  </li>
+  <li>
+    <a href="http://1868alliance.org/">
+      <h3>1868 Hayward Earthquake Alliance</h3>
+      <img src="images/1868.gif" alt="EarthQuake alliance Seal"/>
+    </a>
+    <p>This web site was created to help coordinate and promote efforts and
+		activities between organizations throughout the greater San Francisco Bay
+		Area planning to commemorate the earthquake.</p>
+  </li>
+  <li>
+    <a href="http://seismo.berkeley.edu/~burgmann/RESEARCH/research_HF.html">
+      <h3>The Accumulation of Interseismic Strain Along the Hayward Fault</h3>
+      <img src="images/insar.jpg" alt="Strain Multi Color Map"/>
+    </a>
+    <p>Interseismic surface deformation along the Hayward Fault monitored with
+		Interferometric Synthetic Aperture Radar (InSAR) and the Global
+		Positioning System (GPS).</p>
+  </li>
+  <li>
+    <a href="http://seismo.berkeley.edu/faq/1868_0.html">
+      <h3>Berkeley Seismological Laboratory</h3>
+      <img src="" alt=""/>
+    </a>
+    <p>More information about the earthquake, including location parameters,
+		maps, photos, and seismograms.</p>
+  </li>
+  <li>
+    <a href="http://pubs.usgs.gov/of/1996/0705/of96-705.pdf">
+      <h3>Hayward Fault Subsystem</h3>
+      <img src="" alt=""/>
+    </a>
+    <p>More information about the Hayward fault subsystem and other Bay Area faults.</p>
   </li>
 </ul>
